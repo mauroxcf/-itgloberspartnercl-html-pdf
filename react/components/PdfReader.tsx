63d6b97a2795ed0d4bed147220f00ae1d1cbbe91
mauroxcf <maurox03@gmail.com>
2022-12-28 18:51:02 -0500

    feature: adding proptypes, default types and schema of the component

--- react/components/PdfReader.tsx
+++ react/components/PdfReader.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types';
 
 type Props = {
   pdfUrl: string
@@ -30,6 +31,40 @@ const PdfReader = ({pdfUrl, width, height}: Props) => {
       </div>
     )
   )
+}
+
+PdfReader.propTypes = {
+  pdfUrl: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+}
+
+PdfReader.defaultProps = {
+  pdfUrl: "",
+  width: 320,
+  height: 600
+}
+
+PdfReader.schema = {
+  title: "Pdf reader",
+  type: "object",
+  properties: {
+    pdfUrl: {
+      title: "url del pdf",
+      description: "Agrega por favor la url del pdf",
+      type: "string",
+    },
+    width: {
+      title: "largo del lector de pdf",
+      description: "Agrega por favor un numero para definir el alto",
+      type: "number",
+    },
+    height: {
+      title: "alto del lector de pdf",
+      description: "Agrega por favor un numero para definir el ancho",
+      type: "number",
+    }
+  }
 }
 
 export default PdfReader
